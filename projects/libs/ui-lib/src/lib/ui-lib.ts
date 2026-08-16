00e4246ef6d23cfa6ac7b8ef747ee78a92fecfb5
Kees de Bruin <mare.flies_0d@icloud.com>
2026-08-16 12:24:48 +0200

    Updated to Angular v22

--- projects/libs/ui-lib/src/lib/ui-lib.ts
+++ projects/libs/ui-lib/src/lib/ui-lib.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'lib-ui-lib',
   imports: [],
   template: ` <p>ui-lib works!</p> `,
+  changeDetection: ChangeDetectionStrategy.Eager,
   styles: ``,
 })
 export class UiLib {}
